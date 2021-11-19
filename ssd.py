ed9d83b36de4ee22d0c9809b23f891da9e9776e2
GokouRuri7 <42240705+GokouRuri7@users.noreply.github.com>
2021-11-19 14:49:17 +0800

    Add files via upload

---
+++ ssd.py
@@ -0,0 +1,315 @@
+import colorsys
+import os
+import time
+import warnings
+
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+from PIL import Image, ImageDraw, ImageFont
+
+from nets.ssd import SSD300
+from utils.anchors import get_anchors
+from utils.utils import cvtColor, get_classes, resize_image, preprocess_input
+from utils.utils_bbox import BBoxUtility
+
+warnings.filterwarnings("ignore")
+
+class SSD(object):
+    _defaults = {
+        #--------------------------------------------------------------------------#
+        #   model_path指向logs文件夹下的权值文件，classes_path指向model_data下的txt
+        #   训练好后logs文件夹下存在多个权值文件，选择验证集损失较低的即可。
+        #   验证集损失较低不代表mAP较高，仅代表该权值在验证集上泛化性能较好。
+        #   如果出现shape不匹配，同时要注意训练时的model_path和classes_path参数的修改
+        #--------------------------------------------------------------------------#
+
+        "model_path"        : 'logs/ep098-loss1.788-val_loss4.897.pth',
+        # "model_path"        : 'model_data/ssd_weights.pth',
+        "classes_path"      : 'model_data/cls_classes.txt',
+        
+        #---------------------------------------------------------------------#
+        #   用于预测的图像大小，和train.py使用同一个即可
+        #---------------------------------------------------------------------#
+        "input_shape"       : [512, 512],
+        #-------------------------------#
+        #   主干网络的选择
+        #-------------------------------#
+        "backbone"          : "vgg",
+        #---------------------------------------------------------------------#
+        #   只有得分大于置信度的预测框会被保留下来
+        #---------------------------------------------------------------------#
+        "confidence"        : 0.5,
+        #---------------------------------------------------------------------#
+        #   非极大抑制所用到的nms_iou大小
+        #---------------------------------------------------------------------#
+        "nms_iou"           : 0.45,
+        #---------------------------------------------------------------------#
+        #   用于指定先验框的大小
+        #---------------------------------------------------------------------#
+        'anchors_size'      : [30, 60, 111, 162, 213, 264, 315],
+        #---------------------------------------------------------------------#
+        #   该变量用于控制是否使用letterbox_image对输入图像进行不失真的resize，
+        #   在多次测试后，发现关闭letterbox_image直接resize的效果更好
+        #---------------------------------------------------------------------#
+        "letterbox_image"   : False,
+        #-------------------------------#
+        #   是否使用Cuda
+        #   没有GPU可以设置成False
+        #-------------------------------#
+        "cuda"              : True,
+    }
+
+    @classmethod
+    def get_defaults(cls, n):
+        if n in cls._defaults:
+            return cls._defaults[n]
+        else:
+            return "Unrecognized attribute name '" + n + "'"
+
+    #---------------------------------------------------#
+    #   初始化ssd
+    #---------------------------------------------------#
+    def __init__(self, **kwargs):
+        self.__dict__.update(self._defaults)
+        for name, value in kwargs.items():
+            setattr(self, name, value)
+        #---------------------------------------------------#
+        #   计算总的类的数量
+        #---------------------------------------------------#
+        self.class_names, self.num_classes  = get_classes(self.classes_path)
+        self.anchors                        = torch.from_numpy(get_anchors(self.input_shape, self.anchors_size, self.backbone)).type(torch.FloatTensor)
+        if self.cuda:
+            self.anchors = self.anchors.cuda()
+        self.num_classes                    = self.num_classes + 1
+        
+        #---------------------------------------------------#
+        #   画框设置不同的颜色
+        #---------------------------------------------------#
+        hsv_tuples = [(x / self.num_classes, 1., 1.) for x in range(self.num_classes)]
+        self.colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+        self.colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), self.colors))
+
+        self.bbox_util = BBoxUtility(self.num_classes)
+        self.generate()
+
+    #---------------------------------------------------#
+    #   载入模型
+    #---------------------------------------------------#
+    def generate(self):
+        #-------------------------------#
+        #   载入模型与权值
+        #-------------------------------#
+        self.net    = SSD300(self.num_classes, self.backbone)
+        device      = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.net.load_state_dict(torch.load(self.model_path, map_location=device))
+        self.net    = self.net.eval()
+        print('{} model, anchors, and classes loaded.'.format(self.model_path))
+
+        if self.cuda:
+            self.net = torch.nn.DataParallel(self.net)
+            cudnn.benchmark = True
+            self.net = self.net.cuda()
+
+    #---------------------------------------------------#
+    #   检测图片
+    #---------------------------------------------------#
+    def detect_image(self, image):
+        #---------------------------------------------------#
+        #   计算输入图片的高和宽
+        #---------------------------------------------------#
+        image_shape = np.array(np.shape(image)[0:2])
+        #---------------------------------------------------------#
+        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
+        #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
+        #---------------------------------------------------------#
+        image       = cvtColor(image)
+        #---------------------------------------------------------#
+        #   给图像增加灰条，实现不失真的resize
+        #   也可以直接resize进行识别
+        #---------------------------------------------------------#
+        image_data  = resize_image(image, (self.input_shape[1], self.input_shape[0]), self.letterbox_image)
+        #---------------------------------------------------------#
+        #   添加上batch_size维度，图片预处理，归一化。
+        #---------------------------------------------------------#
+        image_data = np.expand_dims(np.transpose(preprocess_input(np.array(image_data, dtype='float32')), (2, 0, 1)), 0)
+
+        with torch.no_grad():
+            #---------------------------------------------------#
+            #   转化成torch的形式
+            #---------------------------------------------------#
+            images = torch.from_numpy(image_data).type(torch.FloatTensor)
+            if self.cuda:
+                images = images.cuda()
+            #---------------------------------------------------------#
+            #   将图像输入网络当中进行预测！
+            #---------------------------------------------------------#
+            outputs     = self.net(images)
+            #-----------------------------------------------------------#
+            #   将预测结果进行解码
+            #-----------------------------------------------------------#
+            results     = self.bbox_util.decode_box(outputs, self.anchors, image_shape, self.input_shape, self.letterbox_image, 
+                                                    nms_iou = self.nms_iou, confidence = self.confidence)
+            #--------------------------------------#
+            #   如果没有检测到物体，则返回原图
+            #--------------------------------------#
+            if len(results[0]) <= 0:
+                return image
+
+            top_label   = np.array(results[0][:, 4], dtype = 'int32')
+            top_conf    = results[0][:, 5]
+            top_boxes   = results[0][:, :4]
+        #---------------------------------------------------------#
+        #   设置字体与边框厚度
+        #---------------------------------------------------------#
+        font = ImageFont.truetype(font='model_data/simhei.ttf', size=np.floor(3e-2 * np.shape(image)[1] + 0.5).astype('int32'))
+        thickness = max((np.shape(image)[0] + np.shape(image)[1]) // self.input_shape[0], 1)
+        
+        #---------------------------------------------------------#
+        #   图像绘制
+        #---------------------------------------------------------#
+        for i, c in list(enumerate(top_label)):
+            predicted_class = self.class_names[int(c)]
+            box             = top_boxes[i]
+            score           = top_conf[i]
+
+            top, left, bottom, right = box
+
+            top     = max(0, np.floor(top).astype('int32'))
+            left    = max(0, np.floor(left).astype('int32'))
+            bottom  = min(image.size[1], np.floor(bottom).astype('int32'))
+            right   = min(image.size[0], np.floor(right).astype('int32'))
+
+            label = '{} {:.2f}'.format(predicted_class, score)
+            draw = ImageDraw.Draw(image)
+            label_size = draw.textsize(label, font)
+            label = label.encode('utf-8')
+            print(label, top, left, bottom, right)
+            
+            if top - label_size[1] >= 0:
+                text_origin = np.array([left, top - label_size[1]])
+            else:
+                text_origin = np.array([left, top + 1])
+
+            for i in range(thickness):
+                draw.rectangle([left + i, top + i, right - i, bottom - i], outline=self.colors[c])
+            draw.rectangle([tuple(text_origin), tuple(text_origin + label_size)], fill=self.colors[c])
+            draw.text(text_origin, str(label,'UTF-8'), fill=(0, 0, 0), font=font)
+            del draw
+
+        return image
+
+    def get_FPS(self, image, test_interval):
+        #---------------------------------------------------#
+        #   计算输入图片的高和宽
+        #---------------------------------------------------#
+        image_shape = np.array(np.shape(image)[0:2])
+        #---------------------------------------------------------#
+        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
+        #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
+        #---------------------------------------------------------#
+        image       = cvtColor(image)
+        #---------------------------------------------------------#
+        #   给图像增加灰条，实现不失真的resize
+        #   也可以直接resize进行识别
+        #---------------------------------------------------------#
+        image_data  = resize_image(image, (self.input_shape[1], self.input_shape[0]), self.letterbox_image)
+        #---------------------------------------------------------#
+        #   添加上batch_size维度，图片预处理，归一化。
+        #---------------------------------------------------------#
+        image_data = np.expand_dims(np.transpose(preprocess_input(np.array(image_data, dtype='float32')), (2, 0, 1)), 0)
+
+        with torch.no_grad():
+            #---------------------------------------------------#
+            #   转化成torch的形式
+            #---------------------------------------------------#
+            images = torch.from_numpy(image_data).type(torch.FloatTensor)
+            if self.cuda:
+                images = images.cuda()
+            #---------------------------------------------------------#
+            #   将图像输入网络当中进行预测！
+            #---------------------------------------------------------#
+            outputs     = self.net(images)
+            #-----------------------------------------------------------#
+            #   将预测结果进行解码
+            #-----------------------------------------------------------#
+            results     = self.bbox_util.decode_box(outputs, self.anchors, image_shape, self.input_shape, self.letterbox_image, 
+                                                    nms_iou = self.nms_iou, confidence = self.confidence)
+
+        t1 = time.time()
+        for _ in range(test_interval):
+            with torch.no_grad():
+                #---------------------------------------------------------#
+                #   将图像输入网络当中进行预测！
+                #---------------------------------------------------------#
+                outputs     = self.net(images)
+                #-----------------------------------------------------------#
+                #   将预测结果进行解码
+                #-----------------------------------------------------------#
+                results     = self.bbox_util.decode_box(outputs, self.anchors, image_shape, self.input_shape, self.letterbox_image, 
+                                                        nms_iou = self.nms_iou, confidence = self.confidence)
+
+        t2 = time.time()
+        tact_time = (t2 - t1) / test_interval
+        return tact_time
+
+    def get_map_txt(self, image_id, image, class_names, map_out_path):
+        f = open(os.path.join(map_out_path, "detection-results/"+image_id+".txt"),"w") 
+        #---------------------------------------------------#
+        #   计算输入图片的高和宽
+        #---------------------------------------------------#
+        image_shape = np.array(np.shape(image)[0:2])
+        #---------------------------------------------------------#
+        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
+        #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
+        #---------------------------------------------------------#
+        image       = cvtColor(image)
+        #---------------------------------------------------------#
+        #   给图像增加灰条，实现不失真的resize
+        #   也可以直接resize进行识别
+        #---------------------------------------------------------#
+        image_data  = resize_image(image, (self.input_shape[1], self.input_shape[0]), self.letterbox_image)
+        #---------------------------------------------------------#
+        #   添加上batch_size维度，图片预处理，归一化。
+        #---------------------------------------------------------#
+        image_data = np.expand_dims(np.transpose(preprocess_input(np.array(image_data, dtype='float32')), (2, 0, 1)), 0)
+
+        with torch.no_grad():
+            #---------------------------------------------------#
+            #   转化成torch的形式
+            #---------------------------------------------------#
+            images = torch.from_numpy(image_data).type(torch.FloatTensor)
+            if self.cuda:
+                images = images.cuda()
+            #---------------------------------------------------------#
+            #   将图像输入网络当中进行预测！
+            #---------------------------------------------------------#
+            outputs     = self.net(images)
+            #-----------------------------------------------------------#
+            #   将预测结果进行解码
+            #-----------------------------------------------------------#
+            results     = self.bbox_util.decode_box(outputs, self.anchors, image_shape, self.input_shape, self.letterbox_image, 
+                                                    nms_iou = self.nms_iou, confidence = self.confidence)
+            #--------------------------------------#
+            #   如果没有检测到物体，则返回原图
+            #--------------------------------------#
+            if len(results[0]) <= 0:
+                return 
+
+            top_label   = np.array(results[0][:, 4], dtype = 'int32')
+            top_conf    = results[0][:, 5]
+            top_boxes   = results[0][:, :4]
+        
+        for i, c in list(enumerate(top_label)):
+            predicted_class = self.class_names[int(c)]
+            box             = top_boxes[i]
+            score           = str(top_conf[i])
+
+            top, left, bottom, right = box
+            if predicted_class not in class_names:
+                continue
+
+            f.write("%s %s %s %s %s %s\n" % (predicted_class, score[:6], str(int(left)), str(int(top)), str(int(right)),str(int(bottom))))
+
+        f.close()
+        return 
---
+++ train.py
@@ -0,0 +1,235 @@
+import warnings
+
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+from nets.ssd import SSD300
+from nets.ssd_training import MultiboxLoss, weights_init
+from utils.anchors import get_anchors
+from utils.callbacks import LossHistory
+from utils.dataloader import SSDDataset, ssd_dataset_collate
+from utils.utils import get_classes
+from utils.utils_fit import fit_one_epoch
+
+warnings.filterwarnings("ignore")
+ 
+if __name__ == "__main__":
+    #-------------------------------#
+    #   是否使用Cuda
+    #   没有GPU可以设置成False
+    #-------------------------------#
+    Cuda = True
+    #--------------------------------------------------------#
+    #   训练前一定要修改classes_path，使其对应自己的数据集
+    #--------------------------------------------------------#
+    classes_path    = 'model_data/cls_classes.txt'
+    #----------------------------------------------------------------------------------------------------------------------------#
+    #   权值文件的下载请看README，可以通过网盘下载。模型的 预训练权重 对不同数据集是通用的，因为特征是通用的。
+    #   模型的 预训练权重 比较重要的部分是 主干特征提取网络的权值部分，用于进行特征提取。
+    #   预训练权重对于99%的情况都必须要用，不用的话主干部分的权值太过随机，特征提取效果不明显，网络训练的结果也不会好
+    #
+    #   如果训练过程中存在中断训练的操作，可以将model_path设置成logs文件夹下的权值文件，将已经训练了一部分的权值再次载入。
+    #   同时修改下方的 冻结阶段 或者 解冻阶段 的参数，来保证模型epoch的连续性。
+    #   
+    #   当model_path = ''的时候不加载整个模型的权值。
+    #
+    #   此处使用的是整个模型的权重，因此是在train.py进行加载的，下面的pretrain不影响此处的权值加载。
+    #   如果想要让模型从主干的预训练权值开始训练，则设置model_path = ''，下面的pretrain = True，此时仅加载主干。
+    #   如果想要让模型从0开始训练，则设置model_path = ''，下面的pretrain = Fasle，Freeze_Train = Fasle，此时从0开始训练，且没有冻结主干的过程。
+    #   一般来讲，从0开始训练效果会很差，因为权值太过随机，特征提取效果不明显。
+    #
+    #   网络一般不从0开始训练，至少会使用主干部分的权值，有些论文提到可以不用预训练，主要原因是他们 数据集较大 且 调参能力优秀。
+    #   如果一定要训练网络的主干部分，可以了解imagenet数据集，首先训练分类模型，分类模型的 主干部分 和该模型通用，基于此进行训练。
+    #----------------------------------------------------------------------------------------------------------------------------#
+    model_path      = 'model_data/ssd_weights.pth'
+    #------------------------------------------------------#
+    #   输入的shape大小
+    #------------------------------------------------------#
+    input_shape     = [300, 300]
+    #--------------------------------------------#
+    #   vgg或者mobilenetv2
+    #---------------------------------------------#
+    backbone        = "vgg"
+    #----------------------------------------------------------------------------------------------------------------------------#
+    #   是否使用主干网络的预训练权重，此处使用的是主干的权重，因此是在模型构建的时候进行加载的。
+    #   如果设置了model_path，则主干的权值无需加载，pretrained的值无意义。
+    #   如果不设置model_path，pretrained = True，此时仅加载主干开始训练。
+    #   如果不设置model_path，pretrained = False，Freeze_Train = Fasle，此时从0开始训练，且没有冻结主干的过程。
+    #----------------------------------------------------------------------------------------------------------------------------#
+    pretrained      = False
+    #----------------------------------------------------#
+    #   可用于设定先验框的大小，默认的anchors_size
+    #   是根据voc数据集设定的，大多数情况下都是通用的！
+    #   如果想要检测小物体，可以修改anchors_size
+    #   一般调小浅层先验框的大小就行了！因为浅层负责小物体检测！
+    #   比如anchors_size = [21, 45, 99, 153, 207, 261, 315]
+    #----------------------------------------------------#
+    anchors_size    = [30, 60, 111, 162, 213, 264, 315]
+
+    #----------------------------------------------------#
+    #   训练分为两个阶段，分别是冻结阶段和解冻阶段。
+    #   显存不足与数据集大小无关，提示显存不足请调小batch_size。
+    #   受到BatchNorm层影响，batch_size最小为2，不能为1。
+    #----------------------------------------------------#
+    #----------------------------------------------------#
+    #   冻结阶段训练参数
+    #   此时模型的主干被冻结了，特征提取网络不发生改变
+    #   占用的显存较小，仅对网络进行微调
+    #----------------------------------------------------#
+    Init_Epoch          = 0
+    Freeze_Epoch        = 50
+    Freeze_batch_size   = 8
+    Freeze_lr           = 5e-4
+    #----------------------------------------------------#
+    #   解冻阶段训练参数
+    #   此时模型的主干不被冻结了，特征提取网络会发生改变
+    #   占用的显存较大，网络所有的参数都会发生改变
+    #----------------------------------------------------#
+    UnFreeze_Epoch      = 100
+    Unfreeze_batch_size = 4
+    Unfreeze_lr         = 1e-4
+    #------------------------------------------------------#
+    #   是否进行冻结训练，默认先冻结主干训练后解冻训练。
+    #------------------------------------------------------#
+    Freeze_Train        = True
+    #------------------------------------------------------#
+    #   用于设置是否使用多线程读取数据
+    #   开启后会加快数据读取速度，但是会占用更多内存
+    #   内存较小的电脑可以设置为2或者0  
+    #------------------------------------------------------#
+    num_workers         = 6
+    #----------------------------------------------------#
+    #   获得图片路径和标签
+    #----------------------------------------------------#
+    train_annotation_path   = 'taco_train.txt'
+    val_annotation_path     = 'taco_val.txt'
+
+    #----------------------------------------------------#
+    #   获取classes和anchor
+    #----------------------------------------------------#
+    class_names, num_classes = get_classes(classes_path)
+    num_classes += 1
+    anchors = get_anchors(input_shape, anchors_size, backbone)
+
+    model = SSD300(num_classes, backbone, pretrained)
+    if not pretrained:
+        weights_init(model)
+    if model_path != '':
+        #------------------------------------------------------#
+        #   权值文件请看README，百度网盘下载
+        #------------------------------------------------------#
+        print('Load weights {}.'.format(model_path))
+        device          = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        model_dict      = model.state_dict()
+        pretrained_dict = torch.load(model_path, map_location = device)
+        pretrained_dict = {k: v for k, v in pretrained_dict.items() if np.shape(model_dict[k]) == np.shape(v)}
+        model_dict.update(pretrained_dict)
+        model.load_state_dict(model_dict)
+
+    model_train = model.train()
+    if Cuda:
+        model_train = torch.nn.DataParallel(model)
+        cudnn.benchmark = True
+        model_train = model_train.cuda()
+
+    criterion       = MultiboxLoss(num_classes, neg_pos_ratio=3.0)
+    loss_history    = LossHistory("logs/")
+
+    #---------------------------#
+    #   读取数据集对应的txt
+    #---------------------------#
+    with open(train_annotation_path) as f:
+        train_lines = f.readlines()
+    with open(val_annotation_path) as f:
+        val_lines   = f.readlines()
+    num_train   = len(train_lines)
+    num_val     = len(val_lines)
+
+    #------------------------------------------------------#
+    #   主干特征提取网络特征通用，冻结训练可以加快训练速度
+    #   也可以在训练初期防止权值被破坏。
+    #   Init_Epoch为起始世代
+    #   Freeze_Epoch为冻结训练的世代
+    #   Unfreeze_Epoch总训练世代
+    #   提示OOM或者显存不足请调小Batch_size
+    #------------------------------------------------------#
+    if True:
+        batch_size  = Freeze_batch_size
+        lr          = Freeze_lr
+        start_epoch = Init_Epoch
+        end_epoch   = Freeze_Epoch
+
+        optimizer       = optim.Adam(model_train.parameters(), lr, weight_decay = 5e-4)
+        lr_scheduler    = optim.lr_scheduler.StepLR(optimizer, step_size = 1, gamma = 0.94)
+
+        train_dataset   = SSDDataset(train_lines, input_shape, anchors, batch_size, num_classes, train = True)
+        val_dataset     = SSDDataset(val_lines, input_shape, anchors, batch_size, num_classes, train = False)
+
+        gen             = DataLoader(train_dataset, shuffle = True, batch_size = batch_size, num_workers = num_workers, pin_memory=False,
+                                    drop_last=True, collate_fn=ssd_dataset_collate)
+        gen_val         = DataLoader(val_dataset  , shuffle = True, batch_size = batch_size, num_workers = num_workers, pin_memory=False, 
+                                    drop_last=True, collate_fn=ssd_dataset_collate)
+
+        epoch_step      = num_train // batch_size
+        epoch_step_val  = num_val // batch_size
+        
+        if epoch_step == 0 or epoch_step_val == 0:
+            raise ValueError("数据集过小，无法进行训练，请扩充数据集。")
+
+        #------------------------------------#
+        #   冻结一定部分训练
+        #------------------------------------#
+        if Freeze_Train:
+            if backbone == "vgg":
+                for param in model.vgg[:28].parameters():
+                    param.requires_grad = False
+            else:
+                for param in model.mobilenet.parameters():
+                    param.requires_grad = False
+
+        for epoch in range(start_epoch, end_epoch):
+            fit_one_epoch(model_train, model, criterion, loss_history, optimizer, epoch, 
+                    epoch_step, epoch_step_val, gen, gen_val, end_epoch, Cuda)
+            lr_scheduler.step()
+
+    if True:
+        batch_size  = Unfreeze_batch_size
+        lr          = Unfreeze_lr
+        start_epoch = Freeze_Epoch
+        end_epoch   = UnFreeze_Epoch
+
+        optimizer       = optim.Adam(model_train.parameters(), lr, weight_decay = 5e-4)
+        lr_scheduler    = optim.lr_scheduler.StepLR(optimizer, step_size = 1, gamma = 0.94)
+
+        train_dataset   = SSDDataset(train_lines, input_shape, anchors, batch_size, num_classes, train = True)
+        val_dataset     = SSDDataset(val_lines, input_shape, anchors, batch_size, num_classes, train = False)
+
+        gen             = DataLoader(train_dataset, shuffle = True, batch_size = batch_size, num_workers = num_workers, pin_memory=False,
+                                    drop_last=True, collate_fn=ssd_dataset_collate)
+        gen_val         = DataLoader(val_dataset  , shuffle = True, batch_size = batch_size, num_workers = num_workers, pin_memory=False, 
+                                    drop_last=True, collate_fn=ssd_dataset_collate)
+
+        epoch_step      = num_train // batch_size
+        epoch_step_val  = num_val // batch_size
+        
+        if epoch_step == 0 or epoch_step_val == 0:
+            raise ValueError("数据集过小，无法进行训练，请扩充数据集。")
+
+        #------------------------------------#
+        #   解冻后训练
+        #------------------------------------#
+        if Freeze_Train:
+            if backbone == "vgg":
+                for param in model.vgg[:28].parameters():
+                    param.requires_grad = True
+            else:
+                for param in model.mobilenet.parameters():
+                    param.requires_grad = True
+            
+        for epoch in range(start_epoch, end_epoch):
+            fit_one_epoch(model_train, model, criterion, loss_history, optimizer, epoch, 
+                    epoch_step, epoch_step_val, gen, gen_val, end_epoch, Cuda)
+            lr_scheduler.step()
